f66a8a9c1ea206ae803b698389f19259692bdda6
Dragos Dehelean <dragos.dehelean@selenis.ro>
2019-03-02 17:15:30 +0200

    Updates

---
+++ precache-manifest.dc15bf36a5734aca875961362d06073f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e70b7e72b6566f4d265",
+    "url": "/React-Giphy-App/static/js/runtime~main.6e70b7e7.js"
+  },
+  {
+    "revision": "18b80a7f865fd41d6b8b",
+    "url": "/React-Giphy-App/static/js/main.18b80a7f.chunk.js"
+  },
+  {
+    "revision": "a080abdb6f85b3109260",
+    "url": "/React-Giphy-App/static/js/2.a080abdb.chunk.js"
+  },
+  {
+    "revision": "18b80a7f865fd41d6b8b",
+    "url": "/React-Giphy-App/static/css/main.ce6b84c8.chunk.css"
+  },
+  {
+    "revision": "399c6404314fe7283f3341dffe588bd4",
+    "url": "/React-Giphy-App/index.html"
+  }
+];
